1053e31245a9d3daaf322d6591d215b0b350e920
Arun Jamal <jamala6@mymacewan.ca>
2021-06-30 21:03:02 -0600

    Initial Commit

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,18 @@
+﻿/* Purpose: Handle click for altitude mode button */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+	// Button click event
+	public void Button_Click() {
+		// If the camera is not on, then turn it on, otherwise turn it off.
+		if (!GetComponent<Camera> ().isActiveAndEnabled) {
+			GetComponent<Camera> ().enabled = true;
+		} else {
+			GetComponent<Camera> ().enabled = false;
+		}
+	}
+}
---
+++ Assets/CameraController.cs
@@ -0,0 +1,47 @@
+﻿/* Purpose: Adds the controls to move the camera around */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public float speed = 0f;
+
+	// Update is called once per frame
+	void Update () {
+		// If statements are used over else if so the user can move the camera in multiple directions.
+		if(Input.GetKey("d")) {
+			transform.Translate(Vector3.right * speed);
+		}
+		if(Input.GetKey("a")) {
+			transform.Translate(Vector3.left * speed);
+		}
+		if(Input.GetKey("s")) {
+			transform.Translate(Vector3.back * speed);
+		}
+		if(Input.GetKey("w")) {
+			transform.Translate(Vector3.forward * speed);
+		}
+		if (Input.GetKey ("up")) {
+			transform.Rotate(Vector3.left* speed);
+		}
+		if (Input.GetKey ("down")) {
+			transform.Rotate(Vector3.right* speed);
+		}
+		if (Input.GetKey ("left")) {
+			transform.Rotate(Vector3.down* speed);
+		}
+		if (Input.GetKey ("right")) {
+			transform.Rotate(Vector3.up* speed);
+		}
+		if (Input.GetMouseButton (0)) {
+			transform.position = Input.mousePosition;
+		}
+	}
+}
